de37712015c565f8f86991cf0d043808a4a2d598
Erolhalim <erolhalim2@gmail.com>
2022-05-30 13:47:21 +0100

    hh

--- Question-11.py
+++ Question-11.py
@@ -1,11 +1,26 @@
 
+def ReportError(MorseCode):
+
+
+
+
+
 def SendMorseCose(MorseCode):
     plaintext = input("Enter your message (uppercase letters and spaces only)")
-    plaintextlength = len(plaintext)
-    MorseCode = ""
-    for i in range(plaintextlength):
-        plaintextletter = plaintext[i]
-        if plaintextletter == " ":
-            Index = 0
-        else:
-            Index = ord(plaintextletter) - ord('A') + 1
+    if plaintext.isupper == True:
+      plaintextlength = len(plaintext)
+      MorseCode = ""
+      for i in range(plaintextlength):
+          plaintextletter = plaintext[i]
+          if plaintextletter == " ":
+              Index = 0
+          else:
+              Index = ord(plaintextletter) - ord('A') + 1
+    else:
+        ReportError(MorseCode)
+
+
+
+SendMorseCose()
+
+isalpha()
