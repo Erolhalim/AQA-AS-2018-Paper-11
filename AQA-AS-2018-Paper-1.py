1e131d74166d285f783bc40a97d4d1901671eb33
Erolhalim <erolhalim2@gmail.com>
2022-05-30 12:42:51 +0100

    main

---
+++ AQA-AS-2018-Paper-1.py
@@ -0,0 +1,13 @@
+number = 0
+while number < 1 or number > 10:
+    number = int(input("Enter a postive whole number"))
+    if number > 10:
+        print("number too large")
+    else:
+        if number < 1:
+            print("Not a postive number")
+c = 1
+for k in range(0,number-1):
+    print(c)
+c = (c*(number-1-k))//(k+1)
+        
