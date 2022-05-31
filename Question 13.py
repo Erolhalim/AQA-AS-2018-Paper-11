3b8ce618e521ae7031b7207e85d61423f798eeb1
Erolhalim <erolhalim2@gmail.com>
2022-05-31 11:55:03 +0100

    jkhfhgkfdf

---
+++ Question 13.py
@@ -0,0 +1,5 @@
+
+def OutputAlphapbetWithCode():
+    print("A .- B -... C -.-. D -..\nE . F ..-. G --. H ....\nI .. J .--- K -.- L .-..\nM -- N -. O --- P .--.\nQ --.- R .-. S ... T -\nU ..- V ...- W.-- X -..-\nY -.-- Z --..")
+
+OutputAlphapbetWithCode()
