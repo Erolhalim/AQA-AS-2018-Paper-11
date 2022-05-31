c06c4999bf65727a17fac6f1eaf177e3bc31eb90
Erolhalim <erolhalim2@gmail.com>
2022-05-31 11:33:33 +0100

    dszfsd

--- Question-11.py
+++ Question-11.py
@@ -1,15 +1,27 @@
 
-def ReportError(MorseCode):
+
+from pydoc import plain
+
+
+def ReportError(plaintext):
+    if plaintext.replace(' ','').isupper() == True:
+        print("All character have to be uppercase\n")
+    else:
+        print("All character have to be in the alhpabet\n")
+    SendMorseCose()
+
+    
 
 
 
 
 
-def SendMorseCose(MorseCode):
+def SendMorseCose():
     plaintext = input("Enter your message (uppercase letters and spaces only)")
-    if plaintext.isupper == True:
+    if plaintext.replace(' ','').isupper() == True and plaintext.replace(' ','').isalpha() == True :
       plaintextlength = len(plaintext)
       MorseCode = ""
+      print("eorl")
       for i in range(plaintextlength):
           plaintextletter = plaintext[i]
           if plaintextletter == " ":
@@ -17,10 +29,10 @@ def SendMorseCose(MorseCode):
           else:
               Index = ord(plaintextletter) - ord('A') + 1
     else:
-        ReportError(MorseCode)
+        ReportError()
+
 
 
 
 SendMorseCose()
 
-isalpha()
