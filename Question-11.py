d999e8c9e382cd21aada5ed1ac4d4dc096330597
Erolhalim <erolhalim2@gmail.com>
2022-05-31 11:38:06 +0100

    sfs

--- Question-11.py
+++ Question-11.py
@@ -11,19 +11,11 @@ def ReportError(plaintext):
     MorseCode = ""
     
     
-
-    
-
-
-
-
-
 def SendMorseCose():
     plaintext = input("Enter your message (uppercase letters and spaces only)")
     if plaintext.replace(' ','').isupper() == True and plaintext.replace(' ','').isalpha() == True :
       plaintextlength = len(plaintext)
       MorseCode = ""
-      print("eorl")
       for i in range(plaintextlength):
           plaintextletter = plaintext[i]
           if plaintextletter == " ":
