dabff4d1c71c2dd63f0285664f4fa18ab5c66d1b
Erolhalim <erolhalim2@gmail.com>
2022-05-31 12:48:09 +0100

    fsdfsd

--- Question 13.py
+++ Question 13.py
@@ -1,5 +1,19 @@
+def SendReceiveMessages():
+  Dash = [20,23,0,0,24,1,0,17,0,21,0,25,0,15,11,0,0,0,0,22,13,0,0,10,0,0,0]
+  Dot = [5,18,0,0,2,9,0,26,0,19,0,3,0,7,4,0,0,0,12,8,14,6,0,16,0,0,0]
+  Letter = [' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-def OutputAlphapbetWithCode():
-    print("A .- B -... C -.-. D -..\nE . F ..-. G --. H ....\nI .. J .--- K -.- L .-..\nM -- N -. O --- P .--.\nQ --.- R .-. S ... T -\nU ..- V ...- W.-- X -..-\nY -.-- Z --..")
+  MorseCode = [' ','.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..']
+  OutputAlphabetWithTheCode(Letter,MorseCode)
+ 
+  #question 13
+def OutputAlphabetWithTheCode(Letter,MorseCode):
+    counter = 0
+    length = len(Letter) - 1
+    for i in range(length):
+      counter = counter + 1
+      print(Letter[counter],MorseCode[counter], end = ' ')
 
-OutputAlphapbetWithCode()
+
+SendReceiveMessages()
+
