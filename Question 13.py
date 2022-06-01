63a79c3803abce83592914df33e20be7cbcbf8db
Erolhalim <erolhalim2@gmail.com>
2022-06-01 12:03:32 +0100

    GIGIG

--- Question 13.py
+++ Question 13.py
@@ -1,3 +1,17 @@
+from typing import Counter
+
+
+def DisplayMenu():
+  print()
+  print("Main Menu")
+  print("=========")
+  print("R - Receive Morse code")
+  print("S - Send Morse code")
+  print("A - Display")
+  print("X - Exit program")
+  print()
+
+  
 def SendReceiveMessages():
   Dash = [20,23,0,0,24,1,0,17,0,21,0,25,0,15,11,0,0,0,0,22,13,0,0,10,0,0,0]
   Dot = [5,18,0,0,2,9,0,26,0,19,0,3,0,7,4,0,0,0,12,8,14,6,0,16,0,0,0]
@@ -11,9 +25,65 @@ def OutputAlphabetWithTheCode(Letter,MorseCode):
     counter = 0
     length = len(Letter) - 1
     for i in range(length):
-      counter = counter + 1
-      print(Letter[counter],MorseCode[counter], end = ' ')
+      if 3 == counter:
+        counter = counter + 1
+        print(Letter[counter],MorseCode[counter],'\n')
+      elif 7 == counter:
+        counter = counter + 1
+        print(Letter[counter],MorseCode[counter],'\n')
+      elif 11 == counter:
+        counter = counter + 1
+        print(Letter[counter],MorseCode[counter],'\n')
+      elif 15 == counter:
+        counter = counter + 1
+        print(Letter[counter],MorseCode[counter],'\n')
+      elif 19 == counter:
+        counter = counter + 1
+        print(Letter[counter],MorseCode[counter],'\n')
+      elif 23 == counter:
+        counter = counter + 1
+        print(Letter[counter],MorseCode[counter],'\n')
+      else:
+        counter = counter + 1 
+        print(Letter[counter],MorseCode[counter],end = '')
 
 
-SendReceiveMessages()
+def DisplayMenu():
+  print("Main Menu")
+  print("=========")
+  print("R - Receive Morse code")
+  print("S - Send Morse code")
+  print("X - Exit program")
+  print("A - Exit program")
+
+
+def GetMenuOption():
+  MenuOption = ''
+  while len(MenuOption) != 1:
+    MenuOption = input("Enter your choice: ")
+  return MenuOption
+
+
+
+
+DisplayMenu() 
+MenuOption = input("Enter your choice: ")
+if MenuOption == 'R':
+  ReceiveMorseCode(Dash, Letter, Dot)
+elif MenuOption == 'S':
+  SendMorseCode(MorseCode) 
+elif MenuOption == 'X':
+  ProgramEnd = True
+elif MenuOption == 'A':
+  SendReceiveMessages()
+
+
+
+
+
+
+
+
+
+
 
